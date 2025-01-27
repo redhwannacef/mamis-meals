aa654560a38fb64cb6657e0741f580297d84ea1c
Redhwan Nacef <nacefredhwan@gmail.com>
2025-01-27 14:18:32 +0000

    Enable hot reload for recipes
    Simple refresh of full server anytime a recipe changes. Could improve to
    only refresh module if needed in the future.

--- recipesMdPlugin.ts
+++ recipesMdPlugin.ts
@@ -1,22 +1,40 @@
+import { type Plugin } from "vite";
 import fs from "node:fs/promises";
 import graymatter from "gray-matter";
 import markdownit from "markdown-it";
 
-export function recipesMdPlugin() {
+export function recipesMdPlugin(): Plugin {
   const virtualModuleId = "virtual:recipes";
   const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
   return {
     name: "recipes-md",
+
+    configureServer({ watcher }) {
+      watcher.add("recipes");
+    },
+
     resolveId(id: string) {
       if (id === virtualModuleId) {
         return resolvedVirtualModuleId;
       }
     },
+
     async load(id: string) {
       if (id === resolvedVirtualModuleId) {
         const recipeMap = await getRecipes();
         return `export const recipes = ${JSON.stringify(recipeMap)}`;
+      }
+    },
+
+    async handleHotUpdate({ file, server }) {
+      if (file.startsWith("recipes")) {
+        const module = server.moduleGraph.getModuleById(
+          resolvedVirtualModuleId,
+        );
+        if (!module) return;
+        server.moduleGraph.invalidateModule(module);
+        server.ws.send({ type: "full-reload" });
       }
     },
   };
